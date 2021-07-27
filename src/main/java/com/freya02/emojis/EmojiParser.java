49092418aed2e71118d3de3be3d6bf9b24211061
freya022 <41875020+freya022@users.noreply.github.com>
2021-07-27 13:10:59 +0200

    Add docs

--- src/main/java/com/freya02/emojis/EmojiParser.java
+++ src/main/java/com/freya02/emojis/EmojiParser.java
@@ -3,9 +3,22 @@ package com.freya02.emojis;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Utility class to convert unicode emojis to shortcodes and vice versa
+ */
 public class EmojiParser {
 	private static final Pattern SHORTCODE_PATTERN = Pattern.compile(":(.+?|[+-]1):");
 
+	/**
+	 * Replaces unicode emojis by their shortcode equivalent
+	 * <br>Example:
+	 * <br><code>foo 😂 bar</code>
+	 * <br>V
+	 * <br><code>foo :joy: bar</code>
+	 *
+	 * @param str The string with unicode emojis to replace
+	 * @return A new string with the emojis replaced with their shortcodes
+	 */
 	public static String toUnicode(String str) {
 		final StringBuilder sb = new StringBuilder(str.length());
 		final Matcher matcher = SHORTCODE_PATTERN.matcher(str);
