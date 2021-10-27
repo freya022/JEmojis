e10e7270a23f1df318848eaaf03ff30611bb0997
LenderSchlender <LenderSchlender@github.com>
2021-10-27 19:25:28 +0200

    Remove unused import

--- src/main/java/com/freya02/emojis/EmojiStore.java
+++ src/main/java/com/freya02/emojis/EmojiStore.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
